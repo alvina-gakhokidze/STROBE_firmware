2adf183db51b5692405bf246df51fcf1935bab82
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-01-31 09:34:03 -0800

    fixed addresses

--- include/configs/DAC_config.h
+++ include/configs/DAC_config.h
@@ -16,17 +16,26 @@
 #define DAC_ADDRESS 0b1001000 // address 000 and read/write bit set to write to child
 
 #define GEN_CONFIG_REGISTER 0b11010001 //D1h
-#define GEN_CONFIG_MSB 0b00000001 //user-defined, reference datasheet
-#define GEN_CONFIG_LSB 0b11100000 //user-defined, reference datasheet
+#define GEN_CONFIG_WRITE_MSB 0b00010001 //user-defined, reference datasheet
+#define GEN_CONFIG_WRITE_LSB 0b11101000 //user-defined, reference datasheet
 
-#define DAC_DATA_REGISTER 0b00010000 //10h
-#define DAC_DATA_ON_MSB 0b00001111 //temp, for testing
-#define DAC_DATA_ON_LSB 0b11111100 //temp, for testing
+#define GEN_CONFIG_ON_MSB 0b00010001 //user-defined, reference datasheet
+#define GEN_CONFIG_ON_LSB 0b11100000 //user-defined, reference datasheet
+
+
+#define DAC_DATA_REGISTER 0b00100001 //21h
+#define DAC_DATA_ON_MSB 0b11110000 //temp, for testing
+#define DAC_DATA_ON_LSB 0b00011111 //temp, for testing
+#define DAC_DATA_ON 0b00000011
 #define DAC_DATA_OFF 0b00000000
 
 #define TRIGGER_REGISTER 0b11010011 //D3h
 #define TRIGGER_MSB 0b00000000
-#define TRIGGER_LSB 0b00011000
+#define TRIGGER_LSB 0b00010000
+#define SAVE_TO_NVM_MSB 0b00000000
+#define SAVE_TO_NVM_LSB 0b00011000
+#define RELOAD_NVM_MSB 0b00000000
+#define RELOAD_NVM_LSB 0b00101000
 
 #define DAC_RESOLUTION 10
 #define DAC_5V 1023

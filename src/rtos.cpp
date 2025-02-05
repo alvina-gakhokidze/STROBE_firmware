652ec5986ec0ae944ffb3064e7193301283cfc4c
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-02-05 00:17:11 -0800

    minor changes

---
+++ src/rtos.cpp
@@ -0,0 +1,21 @@
+
+// #include <bricks/DAC_handler.h>
+
+
+// void setup()
+// {
+
+
+    
+
+// }
+
+// void loop()
+// {
+//     // "Espressif places most support tasks in CPU 0, leaving CPU 1 for appli- cation use ."
+//     // need to determine stack size for each task
+//     // vTaskDelay() 
+//     vTaskDelete(NULL);
+// }
+
+// // the plan so far. We want to create two tasks, with input 1: power and input 2: flashign frequency. And turn the LED on/off at that rate

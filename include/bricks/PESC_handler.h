2025af90eb7f187d2b88ad032f36ecbf8870b402
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-02-19 23:14:32 -0800

    created files for power extremum seeking controller

---
+++ include/bricks/PESC_handler.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+
+// here we will store the timers that handle the perturbation signal (that will alternate every 50 seconds)
+
+
+
+namespace PowerController
+{
+
+}
---
+++ include/configs/PESC_config.h
@@ -0,0 +1,6 @@
+
+#define SAT_UPPER_LIMIT 5.0 //in volts
+
+#define SAT_LOW_LIMIT 0.0 // in volts
+
+#define PERTURBATION_AMPLITUDE 0.05 // in volts

126f47ae7d150b2fcada4543e2a93545cda2cfc1
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-02-21 11:59:28 -0800

    defined ESC configs

---
+++ include/configs/ESC_config.h
@@ -0,0 +1,13 @@
+
+#define SAT_UPP_LIMIT 5.0 //in volts
+
+#define SAT_LOW_LIMIT 0.0 // in volts
+
+#define POWER_PERTURBATION_AMPLITUDE 0.05 // in volts
+
+#define POWER_GAIN 20.0 // loop gain
+
+#define FREQUENCY_PERTURBATION_AMPLITUDE 0.0 //filler
+
+#define FREQUENCY_GAIN 0.0 // filler
+

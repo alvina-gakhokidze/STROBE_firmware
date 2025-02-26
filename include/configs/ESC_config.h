12558b869036eb7aff9868626872dc1ac1a868ec
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-02-25 18:08:55 -0800

    additional paramters added

--- include/configs/ESC_config.h
+++ include/configs/ESC_config.h
@@ -1,13 +1,17 @@
 
-#define SAT_UPP_LIMIT 5.0 //in volts
+#define SAT_UPP_LIMIT 4.95 //in volts
 
-#define SAT_LOW_LIMIT 0.0 // in volts
+#define SAT_LOW_LIMIT 0.05 // in volts
 
 #define POWER_PERTURBATION_AMPLITUDE 0.05 // in volts
+
+#define POWER_PERTURBATION_PERIOD 50.0 // in seconds
 
 #define POWER_GAIN 20.0 // loop gain
 
 #define FREQUENCY_PERTURBATION_AMPLITUDE 0.0 //filler
 
 #define FREQUENCY_GAIN 0.0 // filler
+
+#define FREQUENCY_PERTURBATION_PERIOD 0.0
 

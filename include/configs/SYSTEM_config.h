34a7f8d01c5ca619e623b676a8fa8ac754f5f42e
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-02-25 18:09:28 -0800

    board limitations defined

--- include/configs/SYSTEM_config.h
+++ include/configs/SYSTEM_config.h
@@ -5,3 +5,16 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 
+
+
+#define CURR_UPP_LIMIT 0.9
+
+#define CURR_LOW_LIMIT 0.1
+
+
+#define FREQ_UPP_LIMIT 100.0
+
+#define FREQ_LOW_LIMIT 0.0
+
+#define POWER_RESISTOR_VALUE 5.0 // in ohms
+// i need to define special case for if frequency is 0

3af13a0d8f389f00c44cb4877c4f3b8cddf4b9da
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-02-25 18:12:01 -0800

    tasks made to loop forever and semaphore checked for if null

--- include/tasks/LED_tasks.h
+++ include/tasks/LED_tasks.h
@@ -1,28 +1,34 @@
 #pragma once
 #include <bricks/LED_handler.h>
 
-namespace ledTasks{
+namespace ledTasks
+{
 
+    /**
+     * @brief creates secondary storage variable we can use to story ledFlyCount... not entirely sure this task is necessary tbh
+     */
     void updateFlyCountTask(void* ledBus)
     {
         strobeLED::LED* localLED = (strobeLED::LED*) ledBus;
-        if(localLED->ledSemaphore != NULL)
+
+        for(;;)
         {
-            if( xSemaphoreTake( localLED->ledSemaphore, (TickType_t) 10 ) == pdTRUE)
+            if(localLED->ledSemaphore != NULL)
             {
-                if(localLED == &strobeLED::redLED)
+                if( xSemaphoreTake( localLED->ledSemaphore, (TickType_t) 5 ) == pdTRUE)
                 {
-                    redLEDFlyCount = localLED->ledFlyCount;
-                }
-                else if(localLED == &strobeLED::blueLED)
-                {
-                    blueLEDFlyCount = localLED->ledFlyCount;
-                }
-            }
-            else{
-                // do nothing, waiting for it to unblock
-            }   
-        } 
+                    if(localLED == &strobeLED::redLED)
+                    {
+                        strobeLED::redLEDFlyCount = localLED->ledFlyCount;
+                    }
+                    else if(localLED == &strobeLED::blueLED)
+                    {
+                        strobeLED::blueLEDFlyCount = localLED->ledFlyCount;
+                    }
+                } 
+            } 
+
+        }
 
     }
 }

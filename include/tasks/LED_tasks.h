a4bb74b29de2d9677aeb9e8d7cd959f734f25000
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-03-10 00:01:29 -0700

    moved i2c communication to tasks

--- include/tasks/LED_tasks.h
+++ include/tasks/LED_tasks.h
@@ -31,4 +31,69 @@ namespace ledTasks
         }
 
     }
+
+    void toggleLED(void* ledBus)
+    {
+        strobeLED::LED* localLED = (strobeLED::LED*) ledBus;
+
+        for(;;)
+        {
+            // first we TAKE semaphore
+            if(localLED->onSemaphore != NULL)
+            {
+                if( xSemaphoreTake( localLED->onSemaphore, (TickType_t) 5 ) == pdTRUE)
+                {
+                    registerTalk::ledControlOn(localLED->busptr, localLED->power);
+                    digitalWrite(DEBUG_LED, HIGH);
+                }
+            }
+            if(localLED->offSemaphore != NULL)
+            {
+                if( xSemaphoreTake( localLED->offSemaphore, (TickType_t) 5 ) == pdTRUE )
+                {
+                    registerTalk::ledOff(localLED->busptr);
+                    digitalWrite(DEBUG_LED, LOW);
+                }
+            }
+        }
+    }
+
+    void flashLED(void* ledBus)
+    {
+        strobeLED::LED* localLED = (strobeLED::LED*) ledBus;
+
+        for(;;)
+        {
+            // first we TAKE semaphore
+            if(localLED->onFlashSemaphore != NULL)
+            {
+                if( xSemaphoreTake( localLED->onFlashSemaphore, (TickType_t) 5 ) == pdTRUE)
+                {
+                    // registerTalk::ledControlOn(localLED->busptr, localLED->power);
+                    // localLED->ledFlyCount++; // increment total number of interactions
+                    // xSemaphoreGive(localLED->ledSemaphore); //unblock task with this function
+                    localLED->trigger();
+                }
+            }
+            if(localLED->offFlashSemaphore != NULL)
+            {
+                if( xSemaphoreTake( localLED->offFlashSemaphore, (TickType_t) 5 ) == pdTRUE )
+                {
+                    //registerTalk::ledOff(localLED->busptr);
+                    localLED->deTrigger();
+                    //registerTalk::ledOff(localLED->busptr);
+                    //xSemaphoreGive(localLED->offSemaphore);
+                    
+                   // digitalWrite(DEBUG_LED, LOW);
+
+                }
+            }
+        }
+    }
+
+    
+    // the problem is that we can't trigger a timer inside of an ISR with the esp32 s3 for some reason
+
+
+
 }

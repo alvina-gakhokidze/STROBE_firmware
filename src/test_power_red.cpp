3035155a1bc30b4c190cfb026539b86b89c3c51d
alvina-gakhokidze <alvina.gakhokidze@gmail.com>
2025-04-08 20:09:03 -0700

    auxilary files for testing only

--- src/test_power_red.cpp
+++ src/test_power_red.cpp
@@ -40,6 +40,25 @@
 // int power = 0;
 // int increment = 1;
 
+
+
+// uint16_t convert_to_16bit_message(int decimal) {
+//     // Step 1: Convert the decimal number into a 10-bit number.
+//     uint16_t ten_bit = (decimal & 0x3FF);  // Mask with 0x3FF to ensure it's 10 bits
+    
+//     // Step 2: Pad with 1s on both sides to create the 16-bit message: 1111 | 10-bit number | 11
+//     uint16_t result = (0xF << 12) | (ten_bit << 2) | 0x3;  // Set 1111 on the left and 11 on the right
+    
+//     return result;
+// }
+// void print_binary(uint16_t number) {
+//     for (int i = 15; i >= 0; i--) {
+//         printf("%d", (number >> i) & 1);
+//     }
+//     printf("\n");
+// }
+
+
 // void setup(){
 //     Serial.begin(115200);
 //     strobeLED::blueLEDBus.begin(BLUE_SDA, BLUE_SCL, I2C_FREQUENCY);
@@ -95,26 +114,39 @@
 //     delay(500);
 //     registerTalk::writeToDAC(&strobeLED::blueLEDBus, DAC_ADDRESS, GEN_CONFIG_REGISTER, GEN_CONFIG_OFF_MSB, GEN_CONFIG_OFF_LSB );
 //     delay(500);
-//     registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, GEN_CONFIG_REGISTER, GEN_CONFIG_ON_MSB, GEN_CONFIG_ON_LSB );
+//     //registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, GEN_CONFIG_REGISTER, GEN_CONFIG_ON_MSB, GEN_CONFIG_ON_LSB );
 //     //registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, DAC_DATA_REGISTER, DAC_DATA_HALF_POWER_MSB, DAC_DATA_HALF_POWER_LSB );
 //     delay(500);
 //     //registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, DAC_DATA_REGISTER, DAC_DATA_OFF, DAC_DATA_OFF);
 // }
 
+
+
 // void loop() {
 
-//     if(power == 1023){
-//         increment = -1;
-//     }
-//     else if(power == 0){
-//         increment = 1;
-//     }
+//     // if(power == 1023){
+//     //     increment = -1;
+//     // }
+//     // else if(power == 0){
+//     //     increment = 1;
+//     // }
 
-//     power += increment;
 
-//     byte lower = power & 0xff;
-//     byte upper = power >> 8;
-//     Serial.print(int(upper << 8 | lower));
+
+
+//     power = 900;
+//     uint16_t temp = convert_to_16bit_message(power);
+//     Serial.print("temp: ");
+//     Serial.println(temp);
+//     byte upper = (temp >> 8) & 0xff;
+//     byte lower = temp & 0xff;
+
+//     // byte lower = power & 0xff;
+//     // byte upper = power >> 8;
+//     Serial.print("power: ");
+//     Serial.println(int(upper << 8 | lower));
+//     Serial.print("half power: ");
+//     Serial.println(int(DAC_DATA_HALF_POWER_MSB << 8 | DAC_DATA_HALF_POWER_LSB));
 //     Serial.print(" ");
 //     Serial.println(power);
 
@@ -142,10 +174,17 @@
 //     unsigned long timestamp;
     
 //     //writeToDAC(&redLEDBus, DAC_ADDRESS, TRIGGER_REGISTER, SAVE_TO_NVM_MSB, SAVE_TO_NVM_LSB );
-//     timestamp = millis();
+    
 //     //while(millis()-timestamp < 5);
+//     registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, GEN_CONFIG_REGISTER, GEN_CONFIG_OFF_MSB, GEN_CONFIG_OFF_LSB);
+    
+//     registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, GEN_CONFIG_REGISTER, GEN_CONFIG_ON_MSB, GEN_CONFIG_ON_LSB);
 //     registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, DAC_DATA_REGISTER, upper, lower);
-
+//     timestamp = millis();
+//     while(millis() - timestamp < 500);
+//     registerTalk::writeToDAC(&strobeLED::redLEDBus, DAC_ADDRESS, GEN_CONFIG_REGISTER, GEN_CONFIG_OFF_MSB, GEN_CONFIG_OFF_LSB);
+//     timestamp = millis();
+//     while(millis() - timestamp < 300);
 //    float frequency = ((millis()-timestamp1));
 //    Serial.println(frequency);
 
